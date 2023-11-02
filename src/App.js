c20de0b109ed6c1344f898421112e3809274578d
aktan77 <aktan.asankulov55@gmail.com>
2023-11-02 23:06:45 +0600

    My first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BinaryDecimalConverter from './companents/Converter'; // Adjust the path to match the actual location of your component
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Number system Converter</h1>
+      <BinaryDecimalConverter />
+    </div>
+  );
+}
+
+export default App;
